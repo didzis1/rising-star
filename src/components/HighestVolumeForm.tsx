d3478e3d7f08c48015b244d9e25d81bf2dc447d8
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-30 17:09:30 +0200

    Form for fetching highest volume BTC

---
+++ src/components/HighestVolumeForm.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { DatePicker } from 'antd';
+
+const { RangePicker } = DatePicker;
+
+const HighestVolumeForm = () => {
+  return <RangePicker style={{ width: '350px' }} />;
+};
+
+export default HighestVolumeForm;
