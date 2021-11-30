0b50b68681453b632111e9bc3e39b467ac23366b
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-11-30 17:08:50 +0200

    Created TimeMachine.tsx

---
+++ src/components/TimeMachine.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Typography } from 'antd';
+
+const { Text } = Typography;
+
+const TimeMachine = () => {
+  return (
+    <div>
+      <Text>Time machine screen</Text>
+    </div>
+  );
+};
+
+export default TimeMachine;
